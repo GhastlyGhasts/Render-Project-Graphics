b695a888a9cf1106381fceca0cc2d740423b29f8
GhastlyGhasts <ghastlycorp@gmail.com>
2026-03-02 21:18:56 -0500

    Added new files and update main

---
+++ Main.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById("MainRender")
+const context = canvas.getContext("2d")
+var imageData =  context.createImageData(500,500)
+var textureData = []
+const tex1 = new Image(500,500)
+tex1.src = "Resource/Textures/Test_Image.png"
+console.log(tex1)
+tex1.onload = function(){context.drawImage(tex1,0,0)}
+
+document.body.appendChild(tex1)
+context.putImageData(imageData,0,0)
+
+// Image Data Array
+// data[i] = R 
+// data[i+1] = G
+// data[i+2] = B
+// data[i+3] = A
+
+for (let i = 0; i<imageData.data.length;i+=4){
+    imageData.data[i] = 112
+    imageData.data[i+1] = 112
+    imageData.data[i+2] = 29
+    imageData.data[i+3] = 255
+    
+}
+
+
+
+// returns the index of the red
+function findIndexOfXY(x, y){
+    let total = (x+y*imageData.width)*4
+    return total
+}
+
+
+function fillPixel(x,y,color){
+    let i = findIndexOfXY(x,y)
+    imageData.data[i]  =color[0]
+    imageData.data[i+1]  =color[1]
+    imageData.data[i+2]  =color[2]
+    imageData.data[i+3]  =color[3]
+   
+    
+   
+}
+
+
+//Point = [x,y]
+function fillLine(point1,point2, color){
+    let m =  (point2[1]-point1[1])/(point2[0]-point1[0])
+    let b =  point1[1]- m*(point1[0])
+    console.log(m + " " + b)
+
+    let minX = Math.min(point1[0], point2[0])
+    let minY = Math.min(point1[1], point2[1])
+    let maxX = Math.max(point1[0], point2[0])
+    let maxY = Math.max(point1[1], point2[1])
+    //add minimum and maximum check
+    for(let i = minX; i<maxX; i++){
+       
+        fillPixel(i, Math.round((m*i) + b), color)
+    }
+}
+fillLine([0,10],[100,100],[255,0,0,255])
+context.putImageData(imageData,0,0)
+
+console.log(imageData.data)
