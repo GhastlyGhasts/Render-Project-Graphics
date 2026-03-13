5e1af6a3575fcc7139d3f2234fa15f708f8b5a33
GhastlyGhasts <ghastlycorp@gmail.com>
2026-03-12 21:11:21 -0400

    working on fixin  stuff

--- Main.js
+++ Main.js
@@ -58,112 +58,42 @@ function sortPoints(points){
 }
 //Point = [x,y]
 function fillLine(point1,point2, color){
-    
-    /*let m =  slope(point1, point2)
-    let b =  point1[1]- m*(point1[0])
-    console.log(m)
-    */
-    let minX = Math.min(point1[0], point2[0])
-    let minY = Math.min(point1[1], point2[1])
-    let maxX = Math.max(point1[0], point2[0])
-    let maxY = Math.max(point1[1], point2[1])
-    //issue could be the min max system
-    /*
-    if (m == Infinity || m== -Infinity){
+   let x0 = +point1[0]
+   let x1 = +point2[0]
+   let y0 = +point1[1]
+   let y1 = +point2[1]
+   let dx = Math.abs(x1-x0)
+   let dy = Math.abs(y1-y0)
+   let sx = Math.sign(x1-x0)
+   let sy = Math.sign(y1-y0)
+   let err = dx-dy
+   let i = 0
+   while(true && i<100000){
+    i++;
+    let err2 = err*2
+    console.log(x1)
+    fillPixel(x0,y0,color)
+        if (Math.round(x0)==Math.round(x1)&&Math.round(y0)==Math.round(y1)){break}
         
-        for(let i = minY; i<maxY; i++){fillPixel(minX,i,color)}
-    }else{
-
-    for(let i = minX; i<maxX; i++){
-       
-        fillPixel(i, Math.round((m*i) + b), color)
-    }} 
-    */
-    
-    
-    let x0 = point1[0]
-    let x1 = point2[0]
-    let y0 = point1[1]
-    let y1 = point2[1]
-    if (x0>x1){
-        let tempx = x1
-        let tempy = y1
-        x1=x0
-        x0=tempx
-        y1 = y0
-        y0=tempy
-    }
-    let dx = x1 - x0
-    let dy = y1 - y0
-    let D = 2*dy - dx
-    let x= x0
-    let y = y0
-    let m = (y1-y0)/(x1-x0)
-    
-
-    if(m == Infinity || m== -Infinity){
-        
-        
-        for(let i = minY; i<maxY; i++){fillPixel(minX,i,color)}
-        
-    }else if((m<-1||m>1)){
-        // iterate over y 
-        for(let y= y0; y<y1;y++){
-            //this runs properly
-            fillPixel(x, y,color)
-            if (D > 0){
-                x = x + 1
-                D = D + (2 * (dx - dy))
-            }else{
-                D = D + 2*dx
-            } 
-            
-        }
-        
-        
-        
-        
-    } else{
-        for (let x  = x0; x<x1; x++){
-            fillPixel(x, y,color)
-            if (D > 0){
-                y = y + 1
-                D = D + (2 * (dy - dx))
-            }else{
-                D = D + 2*dy
-            } 
-        
-        }
-    }
+        if(err2 > -1*dy){err -= dy;x0+=sx}
+        if(err2 < dx){err +=dx;y0+=sy}
+   }
+   
     
 }
 //function convertToXY(xi,yi,z){
    // let x = 
  //   return [x,y]
 //}
- function scanTri(point1i,point2i,point3i,color){
-    let points = sortPoints([point1i,point2i,point3i])
-
-    if (points[0][1]==points[1][1]){
-        point3 = points[2]
-        point1 = points[0]
-        point2 = points[1]
-    }else{
-       point3 = points[0]
-       point1 = points[2]
-       point2 = points[1] 
-    }
-    
-    
-    //console.log(point1,point2,point3)
+function fillTri(point1,point2,point3, color){
     let m1 =  slope(point1, point3)
     let b1 =  point1[1]- m1*(point1[0])
     let m2 =  slope(point2, point3)
     let b2 =  point2[1]- m2*(point2[0])
     minY = Math.min(point3[1],point1[1])
     maxY = Math.max(point3[1],point1[1])
-    
-    
+    let x1 = 0
+    let x2 = 0
     for(let y = minY; y<=maxY; y++){
         // rewrite this to only use integer math
         //probably do something with bresseham 
@@ -180,33 +110,48 @@ function fillLine(point1,point2, color){
 
         
         fillLine([x1, y], [x2, y],color)
-        context.putImageData(imageData,0,0)
+        
         
         //await sleep(100)
         
    }
+}
+ function scanTri(point1i,point2i,point3i,color){
+    let points = sortPoints([point1i,point2i,point3i])
+
+    if (points[0][1]==points[1][1]){
+        point3 = points[2]
+        point1 = points[0]
+        point2 = points[1]
+    }else{
+       point3 = points[0]
+       point1 = points[2]
+       point2 = points[1] 
+    }
+    
+    if (point1[1] == point2[1] || point2[1] == point3[1]||point1[1] == point3[1]){
+        fillTri(point1,point2,point3,color)
+    }else{
+
+        point4 = [Math.round(point1[0] + ((point2[1] - point1[1]) / (point3[1] - point1[1])) * (point3[0] - point1[0])),    point2[1]]
+        console.log(point4)
+        fillTri(point1,point2,point4,color)
+        fillTri(point2,point4,point3,color)
+    }
+    
+    //console.log(point1,point2,point3)
+    
     // not working due to the fact that the rendering needs to be properly implemented
     //for a slope higher than 1 on a slope going negative, investigate line drawing
 
      //tommorow finish line drawing, filling, 3d projection and z buffering.
 }
 
-function fillTri(point1,point2, point3, color){
-   //fillLine(point1,point2,color)
-   //fillLine(point2,point3,color)  
-   //fillLine(point3,point1, color)
 
-
-   // code here to check if iso or equi triangle, which does not require a jointed tri approach
-   
-    scanTri(point1,point2,point3,color)
-  
-   
-}
 
 function fillQuad(point1,point2,point3,point4,color){
-    fillTri(point1,point2,point3,color)
-    fillTri(point1,point3,point4,color)
+    scanTri(point1,point2,point3,color)
+    scanTri(point1,point3,point4,color)
 }
 
 //fillTri([70,0],[300,200], [100,200],[255,0,0,255])
@@ -216,7 +161,7 @@ fillQuad([10,10],[110,10], [110,110], [10,110],[255,0,0,255])
 
 //fillTri([300,0], [300,100], [150,0],[255,0,0,255])
 
-fillTri([300,110], [220,10], [150,110],[255,0,0,255])
+scanTri([300,111], [220,10], [150,110],[255,0,0,255])
 
 //fillLine([50,100],[0,0], [255,0,0,255])
 
